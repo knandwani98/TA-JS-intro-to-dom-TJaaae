cd317773287bb7b94b121e9331c120a9b19d4505
Kushal Nandwani <kushalnandwani98@gmail.com>
2022-02-05 22:39:51 +0530

    v

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,17 +1,25 @@
 // 1. Change the title of the page to `Hello AltCampus!`
+document.title = `Hello AltCampus!`
 
 // 2. Select the element using the children property:
 
 //    - Select the `h1` element and change the value to `Learning DOM`
+document.body.children[0].innerHTML = "Learning DOM"
 
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
+document.body.children[5].children[0].innerHTML = 'all about document'
+
 //    - Select the input element with name `email`
+document.querySelector('email')
 
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
+console.log(document.body.children[5].children.length);
 
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
+let emailInput = document.querySelector("input");
 
 // 5. Select the ul element using class selector and store in `topics`
+document.querySelector('.')
 
 // 6. Select the first label element and store in `label`
 
